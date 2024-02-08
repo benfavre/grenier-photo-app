edab874cbdac9372ff4d6bca1ce77da7fb3239d2
laptop <ben@webdesign29.net>
2024-02-08 21:23:42 +0100

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,12 @@
 import type { Config } from 'drizzle-kit';
-import { env } from '~/env';
 
 export default {
   schema: './src/db/schemas/*.ts',
   out: './src/db/migrations',
   driver: 'turso',
   dbCredentials: {
-    url: env.DB_URL,
-    authToken: env.DB_AUTH_TOKEN,
+    url: process.env.DB_URL || "file:./db.sqlite?mode=memory&cache=shared",
+    authToken: process.env.DB_AUTH_TOKEN,
   },
   verbose: true,
   strict: true,
